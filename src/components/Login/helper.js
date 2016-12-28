23e90834a8b2043c66a3c2b3f467dd832e0852ae
Ray Xie <xierui0423@gmail.com>
2016-12-28 19:49:49 +0800

    remove test code

--- src/components/Login/helper.js
+++ src/components/Login/helper.js
@@ -2,8 +2,6 @@
  * Created by ray.xie on 12/26/2016.
  */
 export const updateLoginForm = (state, loginData) => state.set('loginData', loginData);
-export const updateLoginForm2 = (state, loginData) => state.set('loginData', loginData);
-
 
 
 
