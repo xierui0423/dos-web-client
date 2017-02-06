1a38f89f650ad01a108da4faeb2e269634fbbb26
Ray Xie <xierui0423@gmail.com>
2017-02-06 22:36:00 +0800

    match reducer - create

--- src/components/match/reducers/match.js
+++ src/components/match/reducers/match.js
@@ -3,6 +3,8 @@ import InitialState from '../../../initial-state';
 
 export default (state = InitialState.get('match'), action) => {
     switch (action.type) {
+        case 'CREATE_MATCH_ASYNC_SUCCEED':
+            return Immutable.fromJS(action.match);
         case 'FETCH_MATCH_ASYNC_SUCCEED':
             return Immutable.fromJS(action.match);
         case 'LOGOUT_ASYNC_SUCCEED':
