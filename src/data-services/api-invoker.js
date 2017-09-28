6ebcb5948156fa454c3f1e211ea36ab5f1290c9f
ray xie <xierui0423@gmail.com>
2017-09-28 18:36:50 +0800

    rxjs wip

--- src/data-services/api-invoker.js
+++ src/data-services/api-invoker.js
@@ -1,19 +1,17 @@
 import config from '../config';
 
-export default
-
-(url, method, transformInvokingData) => ({
+export default (url, method, transformInvokingData) => ({
   dataSource: data =>
-        $.ajax({
-          url: config.webServer.apiRoot + url,
-          method,
-          contentType: 'application/json',
-          dataType: 'json',
-          data,
-          xhrFields: {
-            withCredentials: true,
-          },
-        }).then(response => response),
+    $.ajax({
+      url: config.webServer.apiRoot + url,
+      method,
+      contentType: 'application/json',
+      dataType: 'json',
+      data,
+      xhrFields: {
+        withCredentials: true,
+      },
+    }).then(response => response),
 
   transformInvokingData,
 });
